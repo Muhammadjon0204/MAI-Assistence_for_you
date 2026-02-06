00c154228689e21b68460ad652aff06f01c33a6e
Muhammadjon0204 <kosimovmuhamadjon23@gmail.com>
2026-02-06 13:33:31 +0300

    modifided Program.cs

--- backend/MAI.API/Program.cs
+++ backend/MAI.API/Program.cs
@@ -3,18 +3,16 @@ using MAI.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Подключение PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
