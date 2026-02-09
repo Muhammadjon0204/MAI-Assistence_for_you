0f44d974937fbaf9bd39c0b6a127fd2da5230907
Muhammadjon0204 <kosimovmuhamadjon23@gmail.com>
2026-02-09 13:49:21 +0300

    Trying to deploy project in Railway

--- backend/MAI.API/Program.cs
+++ backend/MAI.API/Program.cs
@@ -14,6 +14,15 @@ builder.Services.AddScoped<GeminiService>();     // Сам сервис!
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -27,4 +36,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+app.UseCors("AllowAll");
